cc34043073b8a660b4131d9b237530a14adfee2e
Tomáš Zbedina <tomaszbedina@gmail.com>
2025-07-05 13:20:47 +0200

    K8S microservices

--- web-hello/index.js
+++ web-hello/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import os from 'os'
+import fetch from 'node-fetch'
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,13 @@ app.get("/", (req, res) => {
     const helloMessage = `V2: Hello from the ${os.hostname()}`
     console.log(helloMessage);
     res.send(helloMessage);
+});
+
+app.get("/nginx", async (req, res) => {
+    const url = "http://nginx"
+    const response = await fetch(url);
+    const body = await response.text();
+    res.send(body);
 });
 
 app.listen(PORT, ()=> {console.log("running")});
